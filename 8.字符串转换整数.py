2fc0d7ef91c64cb44aa42adfdc6373b5a2efea78
guopeiqing <guopeiqing@git.com>
2021-03-08 10:49:35 +0800

    字符串转换整数

--- 8.字符串转换整数.py
+++ 8.字符串转换整数.py
@@ -1,20 +1,20 @@
 def myAtoi(s):
     s = s.strip()
-    print('s', s, s[0], s[0].isdecimal())
-    if s[0] not in ['-', '+'] or not s[0].isdecimal():
-        print('11112')
+    import re
+    res = re.match('[+|-]{0,1}\d+', s)
+    if res:
+        temp = res.group()
+        if int(temp) < -2 ** 31:
+            return -2 ** 31
+        elif int(temp) > 2 ** 31 - 1:
+            return 2 ** 31 - 1
+        else:
+            return int(temp)
+    else:
         return 0
-    res = ""
-    for item in s:
-        print('item', item)
-        if item in ['-', '+'] or item.isdecimal():
-            res += item
-
-    return res
 
 
 if __name__ == '__main__':
     s = '   4193 with words'
-    print('原始字符串', s)
     res = myAtoi(s)
     print('res', res)
