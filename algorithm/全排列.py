3ce0d62541312bf93387a5d87518d76d12e650ea
guopeiqing <guopeiqing@git.com>
2021-03-08 16:12:40 +0800

    扩展代码解释

--- algorithm/全排列.py
+++ algorithm/全排列.py
@@ -1,15 +1,22 @@
 def AllRange(listx, start, end):
+    """
+    :param listx: 元素列表
+    :param start: 起始位置
+    :param end: 终止位置
+    :return:
+    """
     if start == end:
-        for i in listx:
-            print(i, end='')
-        print()
-    for m in range(start, end + 1):
+        print('元素', listx)
+        # for i in listx:
+        #     print(i, end='')
+        # print()
+    for m in range(start, end):
         # 每次移动下标，改变列表元素
         listx[m], listx[start] = listx[start], listx[m]
         AllRange(listx, start + 1, end)
-        # 恢复为初试状态
+        # 恢复为初试状态,恢复现场【主函数中的形式，方便下一次递归的初始形态一样】
         listx[m], listx[start] = listx[start], listx[m]
 
 
 list1 = [1, 2, 3, 4]
-AllRange(list1, 0, 3)
+AllRange(list1, 0, 4)
