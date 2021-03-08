44bc70f941b72e6ee02233790ee2763305b310d2
guopeiqing <guopeiqing@git.com>
2021-03-08 16:29:22 +0800

    扩展代码解释，增加去重排列的问题

--- algorithm/全排列.py
+++ algorithm/全排列.py
@@ -10,13 +10,17 @@ def AllRange(listx, start, end):
         # for i in listx:
         #     print(i, end='')
         # print()
+    flag_list = []
     for m in range(start, end):
-        # 每次移动下标，改变列表元素
-        listx[m], listx[start] = listx[start], listx[m]
-        AllRange(listx, start + 1, end)
-        # 恢复为初试状态,恢复现场【主函数中的形式，方便下一次递归的初始形态一样】
-        listx[m], listx[start] = listx[start], listx[m]
+        # 标记去重
+        if listx[m] not in flag_list:
+            flag_list.append(listx[m])
+            # 每次移动下标，改变列表元素
+            listx[m], listx[start] = listx[start], listx[m]
+            AllRange(listx, start + 1, end)
+            # 恢复为初试状态,恢复现场【主函数中的形式，方便下一次递归的初始形态一样】
+            listx[m], listx[start] = listx[start], listx[m]
 
 
-list1 = [1, 2, 3, 4]
-AllRange(list1, 0, 4)
+list1 = [1, 2, 3, 3, 5]
+AllRange(list1, 0, 5)
