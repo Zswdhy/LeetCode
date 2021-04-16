63f05a9dae4d912c07599e1185843562b1c32b29
guopeiqing <760766146@qq.com>
2021-04-16 17:19:12 +0800

    动态规划 -- 硬币问题和背包问题

--- algorithm/马士兵/conis.py
+++ algorithm/马士兵/conis.py
@@ -16,9 +16,42 @@ def coinChange(coins, amount):
     return dp(amount)
 
 
+def process(weights, values, index, curWeight, bag):
+    """
+    :param weights: 重量列表
+    :param values:  价值列表
+    :param index:   当前索引值
+    :param curWeight:   当前重量
+    :param bag:         背包重量
+    :return: 最大值
+    """
+    # 超过背包重量
+    if curWeight > bag:
+        return -1
+    # 当前 index 达到最大值
+    if index == len(weights):
+        return 0
+
+    # 忽略本次的重量以及价值
+    p1 = process(weights, values, index + 1, curWeight, bag)
+
+    # 计算本次的重量以及价值
+    p2Next = process(weights, values, index + 1, curWeight + weights[index], bag)
+    p2 = -1
+    if p2Next != -1:
+        p2 = p2Next + values[index]
+    return max(p1, p2)
+
+
 if __name__ == '__main__':
     coins = [1, 2, 5]
     amount = 11
 
     a = coinChange(coins, amount)
     print(a)
+
+    weights = [5, 4, 3, 14, 12, 9, 4, 3]
+    values = [3, 4, 5, 10, 9, 8, 2, 1]
+    bag = 30
+    res = process(weights, values, 0, 0, bag)
+    print('max values', res)
