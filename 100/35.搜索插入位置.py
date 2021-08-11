df42479bbfb0b5fccd5015934b65628046912886
guopeiqing <peiqing.guo@mail.hypers.com>
2021-08-11 10:38:21 +0800

    格式化代码

--- 100/35.搜索插入位置.py
+++ 100/35.搜索插入位置.py
@@ -5,16 +5,16 @@ def search_insert(nums: list, target: int) -> int:
     :param target:
     :return:
     """
-    l, r = 0, len(nums) - 1
-    while l <= r:
-        mid = (l + r) // 2
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
         if target == nums[mid]:
             return mid
         elif target < nums[mid]:
-            r = mid - 1
+            right = mid - 1
         elif target > nums[mid]:
-            l = mid + 1
-    return l
+            left = mid + 1
+    return left
 
 
 if __name__ == '__main__':
