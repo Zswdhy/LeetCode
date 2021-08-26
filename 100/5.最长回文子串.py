5cd800fa0b9b44e7ba9ce5e8723996b38f6807d1
guopeiqing <peiqing.guo@mail.hypers.com>
2021-08-26 11:12:57 +0800

    feat:更新对最长回文子串的理解认知

--- 100/5.最长回文子串.py
+++ 100/5.最长回文子串.py
@@ -3,19 +3,24 @@ def longestPalindrome(s):
     # 哨兵
     matrix = [[False] * n for i in range(n)]
     ans = ""
+    """ l 只用于循环的次数，以及和 ans 长度进行比较.  """
     for l in range(n):
-
         for i in range(n):
             j = i + 1
+            # 结束循环
             if j >= len(ans):
                 break
+            # 第一次进来，默认为 True
             if l == 0:
                 matrix[i][j] = True
+            # 第二次进来，需要进行比较
             elif l == 1:
                 matrix[i][j] = (s[i] == s[j])
+            # 其他情况
             else:
+                # 用 i+1:j-1 && i == j 判断是否为回文串
                 matrix[i][j] = (matrix[i + 1][j - 1] and s[i] == s[j])
-            # 更新子串
+            # 长度大于 ans 更新子串
             if matrix[i][j] and l + 1 > len(ans):
                 ans = matrix[i:j + 1]
     return ans
