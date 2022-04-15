9170f65effd5a140165ce7b1a4c3c7b908ff6527
peiqing.guo <15225967756@sina.cn>
2022-04-15 15:57:45 +0800

    pref:外观数列性能优化，将for循环修改为while循环

--- 100/38.外观数列.py
+++ 100/38.外观数列.py
@@ -24,6 +24,27 @@ def countAndSay(n: int) -> str:
         return _res
 
 
+def countAndSayV2(n: int) -> str:
+    if n == 1:
+        return "1"
+    else:
+        _res = "1"
+        for i in range(1, n):
+            cur_pos, start = 0, 0
+            __res = ""
+            while cur_pos < len(_res):
+                # 直接在循环的时候判断，是否符合情况
+                while cur_pos < len(_res) and _res[cur_pos] == _res[start]:
+                    cur_pos += 1
+                __res += str(cur_pos - start) + _res[start]
+                start = cur_pos
+            _res = __res
+
+        return _res
+
+
 if __name__ == '__main__':
     res = countAndSay(5)
+    print(res)
+    res = countAndSayV2(5)
     print(res)
